31ac0468b2d1eed2c8655fcfd207aeb43fe375bc
Vryon Antonio <vryonantonio@gmail.com>
2025-03-11 15:35:21 +0800

    crunch removed

--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -8,9 +8,7 @@ function About({ aboutRef }) {
           We believe food should be exciting. Our journey started with a love
           for pizza and a craving for something delicious. That’s when we
           created the Pizza Spring Roll.{" "}
-          <span>
-            A snack that’s portable, flavorful, and irresistibly crunchy.
-          </span>
+          <span>A snack that’s portable, flavorful, and irresistible.</span>
         </p>
       </div>
     </div>
