ab4d3c6e3a29f75f5953d878a6dccc6e401e195e
Vryon Antonio <vryonantonio@gmail.com>
2025-03-14 21:15:29 +0800

    animation added

--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -1,15 +1,23 @@
 import styles from "./About.module.css";
+import { motion } from "motion/react";
 
 function About({ aboutRef }) {
   return (
     <div className={styles.container} ref={aboutRef}>
       <div className={styles.innerContainer}>
-        <p>
-          We believe food should be exciting. Our journey started with a love
-          for pizza and a craving for something delicious. That’s when we
-          created the Pizza Spring Roll.{" "}
-          <span>A snack that’s portable, flavorful, and irresistible.</span>
-        </p>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }} // Start hidden and slightly below
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className={styles.framer}
+        >
+          <p>
+            We believe food should be exciting. Our journey started with a love
+            for pizza and a craving for something delicious. That’s when we
+            created the Pizza Spring Roll.{" "}
+            <span>A snack that’s portable, flavorful, and irresistible.</span>
+          </p>
+        </motion.div>
       </div>
     </div>
   );
--- src/pages/Home/HomeHero.jsx
+++ src/pages/Home/HomeHero.jsx
@@ -1,20 +1,28 @@
 import styles from "./HomeHero.module.css";
+import { motion } from "motion/react";
 
 function HomeHero({ homeRef, orderRef }) {
   return (
     <div className={styles.container} ref={homeRef}>
       <div className={styles.innerContainer}>
-        <div className={styles.content}>
-          <h1>Baked, Cheesy, Delicious</h1>
-          <p>Only Pizza Spring Roll in Sampaloc</p>
-          <button
-            onClick={() => {
-              orderRef.current?.scrollIntoView({ behavior: "smooth" });
-            }}
-          >
-            Order now
-          </button>
-        </div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }} // Start hidden and slightly below
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className={styles.framer}
+        >
+          <div className={styles.content}>
+            <h1>Baked, Cheesy, Delicious</h1>
+            <p>Only Pizza Spring Roll in Sampaloc</p>
+            <button
+              onClick={() => {
+                orderRef.current?.scrollIntoView({ behavior: "smooth" });
+              }}
+            >
+              Order now
+            </button>
+          </div>
+        </motion.div>
       </div>
     </div>
   );
